0efdd101a22e95608e1570340faf99cefbef2aa9
nolan <nolan796@live.ca>
2019-11-13 15:47:59 -0700

    Remove whitespace from schema.sql

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -20,10 +20,10 @@ CREATE TABLE properties (
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER  NOT NULL DEFAULT 0,
-  parking_spaces INTEGER  NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
-  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE guest_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  reservation_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
